1c52e648a1f603ed4976f2b0c4150032dc940917
Janella <janellavos486@gmail.com>
2025-04-03 19:20:12 -0700

    Updated frontend files

--- src/Services/AuthService.tsx
+++ src/Services/AuthService.tsx
@@ -2,33 +2,21 @@ import axios from "axios";
 import { UserProfileToken } from "../Models/User";
 import { handleError } from "../Helpers/ErrorHandler";
 
-const api = "http://localhost:5167/api/";
+const api = "https://princess-stock-api-a2c7e16cc89b.herokuapp.com/api/";
 
-export const loginAPI = async (username: string, password: string) => {
-  try {
-    const data = await axios.post<UserProfileToken>(api + "account/login", {
-      username: username,
-      password: password,
-    });
-    return data;
-  } catch (error) {
-    handleError(error);
-  }
-};
+    export const loginAPI = async (username: 
+    string, password: string) => {try {
+    const data = await axios.post<UserProfileToken>
+    (api + "account/login", {username: username,
+    password: password,}); return data;} 
+    catch (error) {handleError(error);}};
 
-export const registerAPI = async (
-  email: string,
-  username: string,
-  password: string
-) => {
-  try {
-    const data = await axios.post<UserProfileToken>(api + "account/register", {
-      email: email,
-      username: username,
-      password: password,
-    });
-    return data;
-  } catch (error) {
-    handleError(error);
-  }
-};
+    export const registerAPI = async (email: string,  
+    username: string, password: string) => {try {
+    const data = await axios.post<UserProfileToken>
+    (api + "account/register", 
+    {email: email, username: username, password: password,});
+    return data;} catch (error) {
+    handleError(error);}};
+
+

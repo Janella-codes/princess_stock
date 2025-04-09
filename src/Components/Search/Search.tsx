560318c0ad575b45da7c3912baccf2d0ff809b03
Janella <janellavos486@gmail.com>
2025-04-08 18:41:01 -0700

    style tweaking

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,31 +1,34 @@
-import { ChangeEvent, SyntheticEvent, useState } from "react";
+import { ChangeEvent, SyntheticEvent } from "react";
 
 interface Props {
-    search: string | undefined;
-    handleSearchChange: (e: ChangeEvent<HTMLInputElement>) => void;
-    onSearchSubmit: (e: SyntheticEvent) => void;
+  search: string | undefined;
+  handleSearchChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onSearchSubmit: (e: SyntheticEvent) => void;
 }
 
-const Search : React.FC<Props> = ({search, handleSearchChange, onSearchSubmit}: Props): JSX.Element => {
-    
-    return (
-        <section className="bg-#97c0db">
-        <div className="max-w-4xl mx-auto p-6 space-y-6">
-          <form
-            className="form relative flex  w-full p-10  bg-darkBlue rounded-lg md:flex-row md:space-y-0 md:space-x-3"
-            onSubmit={onSearchSubmit}
-          >
-            <input
-              className="flex-1 p-3 border-2 rounded-lg placeholder-black focus:outline-none"
-              id="search-input"
-              placeholder="Search companies"
-              value={search}
-              onChange={handleSearchChange}
-            ></input>
-          </form>
-        </div>
-      </section>
-    );
-}
+const Search: React.FC<Props> = ({ search, handleSearchChange, onSearchSubmit }: Props): JSX.Element => {
+  return (
+    <section className="max-w-4xl mx-auto bg-cyan-500 dark:bg-gray-900">
+      <div className="max-w-4xl mx-auto p-6 space-y-6">
+        <form
+          className="form relative flex w-full p-10 rounded-lg md:flex-row md:space-y-0 md:space-x-3"
+          style={{
+            background: "radial-gradient(circle, #97c0db 10%, transparent 10%)",
+            backgroundSize: "20px 20px",
+          }}
+          onSubmit={onSearchSubmit}
+        >
+          <input
+            className="flex-1 p-3 border-2 rounded-lg placeholder-black focus:outline-none"
+            id="search-input"
+            placeholder="Search companies"
+            value={search}
+            onChange={handleSearchChange}
+          />
+        </form>
+      </div>
+    </section>
+  );
+};
 
 export default Search;

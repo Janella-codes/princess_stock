f7f09be18ec098ad8f366a368e073bba109bff15
Janella <janellavos486@gmail.com>
2025-04-09 15:54:20 -0700

    style tweaking

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -31,7 +31,7 @@ const Search: React.FC<Props> = ({ search, handleSearchChange, onSearchSubmit }:
 
   return (
     <>
-      <section className="max-w-4xl mx-auto bg-customCyan dark:bg-gray-900 rounded-lg shadow-md mt-10 mb-10">
+      <section className="max-w-4xl mx-auto bg-fuchsia-300 dark:bg-gray-900 rounded-lg shadow-md mt-10 mb-10">
         <div className="max-w-4xl mx-auto p-6 space-y-6">
           <form
             className="form relative flex w-full p-10 rounded-lg md:flex-row md:space-y-0 md:space-x-3"
